cdcfce9eec692533b7a02b39b7bcedafca02cc6d
catthug92 <itnikitins@gmail.com>
2023-07-24 13:58:49 +0300

    create project

---
+++ MVP+SOLID+DI+Tests+Router/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVP+SOLID+DI+Tests+Router
+//
+//  Created by Никитин Артем on 24.07.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
